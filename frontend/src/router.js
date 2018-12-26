3f5f4d56e4eb80292a76ddea291052a8f065b4b7
maks <it.vimax@gmail.com>
2018-12-26 06:02:50 +0200

    added auth pages: fix bug

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,28 +5,28 @@ import Home from './views/Home.vue'
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('./views/Login.vue')
-        },
-        {
-            path: '/signup',
-            name: 'signup',
-            component: () => import('./views/SignUp.vue')
-        }
-    ]
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('./views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('./views/SignUp.vue')
+    }
+  ]
 })
