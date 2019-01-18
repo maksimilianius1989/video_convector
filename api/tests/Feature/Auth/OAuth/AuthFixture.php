39dc7b8b0ca0ff31948834fc0f314b830062f975
Maks B <it.vimax@gmail.com>
2019-01-18 17:51:38 +0200

    tests

---
+++ api/tests/Feature/Auth/OAuth/AuthFixture.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Api\Test\Feature\Auth\OAuth;
+
+
+use Api\Model\User\Entity\User\ConfirmToken;
+use Api\Model\User\Entity\User\Email;
+use Api\Test\Builder\User\UserBuilder;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AuthFixture extends AbstractFixture
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = (new UserBuilder())
+            ->withDate($now = new \DateTimeImmutable())
+            ->withEmail(new Email('oauth@example.com'))
+            ->withPasswordHash('$2y$12$qwnND33o8DGWvFoepotSju7eTAQ6gzLD/zy6W8NCVtiHPbkybz.w6') // 'password'
+            ->withConfirmToken(new ConfirmToken($token = 'token', $now->modify('+1 day')))
+            ->build();
+
+        $user->confirmSignup($token, new \DateTimeImmutable());
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
